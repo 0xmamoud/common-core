da0cd489b790b34c267213f248da994c1ff09567
mamoud <mamad2k18@gmail.com>
2024-03-15 21:52:23 +0100

    fix: void argument segfault

--- pipex/src/parsing/parse_cmd.c
+++ pipex/src/parsing/parse_cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_cmd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkane <mkane@student.42.fr>                +#+  +:+       +#+        */
+/*   By: kane <kane@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 07:25:17 by mkane             #+#    #+#             */
-/*   Updated: 2024/03/15 11:56:04 by mkane            ###   ########.fr       */
+/*   Updated: 2024/03/15 21:13:27 by kane             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,17 @@
 
 char	ft_parse_cmd(char **argv, t_pipex *pipex)
 {
-	int	i;
-	int	j;
-
-	i = 0;
+	int (i) = 0;
+	int (j) = 0;
 	pipex->cmd = malloc(sizeof(char **) * pipex->len_cmd);
 	if (!pipex->cmd)
 		return (0);
 	while (i < pipex->len_cmd)
 	{
 		pipex->cmd[i] = ft_split(argv[i + 2], ' ');
-		if (!pipex->cmd[i])
+		if (!pipex->cmd[i] || !pipex->cmd[i][0])
 		{
-			j = 0;
-			while (j < i)
+			while (j <= i)
 			{
 				free(pipex->cmd[j]);
 				j++;
